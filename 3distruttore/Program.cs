9a9c5bd0a93501480e873f1a4d6569c20673057e
ste46751 <stefanquarti060@gmail.com>
2023-11-21 10:52:26 +0100

    Aggiungere i file di progetto.

---
+++ 3distruttore/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3distruttore
+{
+    internal class Program
+    {
+        public class esempio
+        {
+            private string nome;
+            private int numero;
+
+            //costruttore senza parametri
+            public esempio()
+            {
+                nome = "Farro";
+                numero = 10;
+            }
+
+            
+            public void stampa()
+            {
+                Console.WriteLine($"Il prezzo di {nome} è {numero} ");
+            }
+            ~esempio()
+            {
+                Console.WriteLine($"{nome}  {numero} è stato eliminato");
+            }
+
+            public void Dispose()
+            {
+                GC.SuppressFinalize(this);
+            }
+        }
+        static void Main(string[] args)
+        {
+            esempio esempio = new esempio();
+            esempio.stampa();
+            
+
+        }
+    }
+}
